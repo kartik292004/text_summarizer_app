91da7d01ba360535b1a6f415b15437aed29e1c75
Goyal <goyalkrati13@gmail.com>
2025-05-02 23:45:57 +0530

    Add requirements.txt and Procfile

--- app.py
+++ app.py
@@ -15,6 +15,4 @@ def summarize():
     return render_template("index.html", summary=summary)
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
